f5e1bf0aeeb23840bbfe4664d9a807417be89b00
Isabella Marinho <isabellamarcelomarinho@gmail.com>
2022-01-12 14:52:19 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Form from "./pages/Form";
+import Listing from "./pages/Listing";
 
 
 function App() {
   return (
-  <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
