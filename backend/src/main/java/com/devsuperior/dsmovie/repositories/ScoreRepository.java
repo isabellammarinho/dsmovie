e2b7723e7257ad55b239e7b9001600e747bd2817
Isabella Marinho <isabellamarcelomarinho@gmail.com>
2022-01-14 21:34:38 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/ScoreRepository.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.ScorePK;
+
+	
+
+	public interface ScoreRepository extends JpaRepository<Score, ScorePK> {
+
+		
+	}
+	
